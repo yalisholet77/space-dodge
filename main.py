033f3b0de052ca070ca095a05e524fee789d052e
AlzyWelzy <welzyalzy@gmail.com>
2023-03-15 22:52:14 +0530

    added a formatter

--- main.py
+++ main.py
@@ -56,8 +56,7 @@ def main():
         if star_count > star_add_increment:
             for _ in range(3):
                 star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                star = pygame.Rect(star_x, -STAR_HEIGHT,
-                                   STAR_WIDTH, STAR_HEIGHT)
+                star = pygame.Rect(star_x, -STAR_HEIGHT,STAR_WIDTH, STAR_HEIGHT)
                 stars.append(star)
 
             star_add_increment = max(200, star_add_increment - 50)
