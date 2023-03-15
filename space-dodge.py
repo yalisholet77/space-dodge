7eea2d071d4c9a9da622b895919e249c2e33a634
AlzyWelzy <welzyalzy@gmail.com>
2023-03-16 00:21:07 +0530

    adding more content

--- space-dodge.py
+++ space-dodge.py
@@ -7,11 +7,12 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Dodge")
 
-BG = pygame.image.load("bg.jpeg")
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 
 def draw():
     WIN.blit(BG, (0, 0))
+    pygame.display.update()
 
 
 def main():
@@ -22,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw()
 
     pygame.quit()
     sys.exit()
