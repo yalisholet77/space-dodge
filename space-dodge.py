f37c10e80e52200b0978837f9962b23ac611068f
AlzyWelzy <welzyalzy@gmail.com>
2023-03-16 21:19:03 +0530

    final commit

--- space-dodge.py
+++ space-dodge.py
@@ -85,6 +85,19 @@ def main():
                 hit = True
                 break
 
+        if hit:
+            lost_text = FONT.render("You Lost!", 1, "white")
+            WIN.blit(
+                lost_text,
+                (
+                    WIDTH / 2 - lost_text.get_width() / 2,
+                    HEIGHT / 2 - lost_text.get_height() / 2,
+                ),
+            )
+            pygame.display.update()
+            pygame.time.delay(4000)
+            break
+
         draw(player, elapsed_time, stars)
 
     pygame.quit()
